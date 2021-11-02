51eed57f7418ef039ee7deff6e0556e546d4afb1
Loncerasjakerei@gmail.com <Loncerasjakerei@gmail.com>
2021-11-02 08:52:59 +0800

    Add key press to close the window

--- OpenglWorld/src/Main.cpp
+++ OpenglWorld/src/Main.cpp
@@ -5,58 +5,86 @@
 #include <iostream>
 
 
-void resizeRenderView(GLFWwindow* window, int width, int height);
 
+
+void resizeRenderView(GLFWwindow* window, int width, int height);
+void closeWindowEvent(GLFWwindow* window);
+
+constexpr int WIDTH{ 1000 };
+constexpr int HEIGHT{ 800 };
+
+enum class WindowOption {
+	OPEN = 0,
+	CLOSE = 1
+};
 
 int main(void)
 {
 
-	// Intialize GLFW 
+
+	// GLFW init
 	glfwInit();
+
 
 	// Config
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-
 	// Create window
-	GLFWwindow* window = glfwCreateWindow(1000, 800, "Reiend", NULL, NULL);
-
+	GLFWwindow* window{ glfwCreateWindow(WIDTH, HEIGHT, "Reiend", NULL, NULL) };
 	if (window == NULL) {
-		std::cout << "Creating GLFW window fail\n";
+		std::cout << "GLFW window: ERROR\n";
 		glfwTerminate();
+		return -1;
 	}
-
 	glfwMakeContextCurrent(window);
 
-	// Initialize GLAD: Check OS
+	// GLAD init
 	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
-		std::cout << "GLAD init: Failed\n";
+		std::cout << "GLAD init: ERROR\n";
+		glfwTerminate();
 		return -1;
 	}
 
-	// Render Window size
-	// -1 to 1
-	// 1000 : 800
-	glViewport(0, 0, 1000, 800);
-
-
-	//Resize render view according to window'size
+	// Render viewport
+	glViewport(0, 0, WIDTH, HEIGHT);
+	
+	// Resize render view relative to window
 	glfwSetFramebufferSizeCallback(window, resizeRenderView);
 
 
-	// Render loop
-	while (!glfwWindowShouldClose(window)) {
+	while (!glfwWindowShouldClose(window)) {	
+
+		// Press ESC to close the window
+		closeWindowEvent(window);
+
+
+		// Clear Screen
+		glClearColor(0.1f, 0.4f, 0.4f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		// Use color value for GLFW window
 		glfwSwapBuffers(window);
+
+		// Handle Events
 		glfwPollEvents();
 	}
-	
-	glfwTerminate();
+
+
 	return 0;
 }
 
 
-void resizeRenderView(GLFWwindow* window, int windowWidth, int windowHeight) {
-	glViewport(0, 0, windowWidth, windowHeight);
+// Resize render view according to window's
+void resizeRenderView(GLFWwindow* window, int width, int height) {
+	glViewport(0, 0, width, height);
+}
+
+
+// Process window close event
+void closeWindowEvent(GLFWwindow* window) {
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+		glfwSetWindowShouldClose(window, static_cast<int>(WindowOption::CLOSE));
+	}
 }
